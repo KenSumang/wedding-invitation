8e4b358815e70a24a7c62a421df27c409dcb6086
KenSumang <ksumang77@gmail.com>
2026-09-13 23:33:58 +0800

    Change banner background and style

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,22 +2,30 @@ import react from 'react';
 
 function Banner() {
     return (
-        <section className="banner w-full h-dvh bg-[url('./src/assets/Banner-new.png')] bg-no-repeat md:bg-cover bg-center">
-            <div className="container max-w-full px-4 sm:px-6 max-w-380">
-                <div className="wrapper w-full h-full">
-                    <div className="flex flex-col items-center gap-2 pb-3 pt-19 md:scale-150 lg:scale-200">
+        <section className="banner w-full h-dvh bg-[url('./src/assets/Banner-new.png')] bg-[length:auto_100%] md:bg-cover bg-center">
+            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent via-60% to-black z-0"></div>
+            <div className="container relative z-10 max-w-full h-full px-4 sm:px-6 max-w-380">
+                <div className="wrapper w-full h-full flex flex-col">
+                    
+                    <div className="flex flex-col items-center gap-5 mt-auto mb-20">
 
-                        <p className="subtitle text-[10px] tracking-wider text-white">TOGETHER FOREVER</p>
+                        <p className="subtitle text-[14px] tracking-wider text-white">TOGETHER FOREVER</p>
 
-                        <div className="banner-title flex flex-col items-center gap-[3px]">
-                            <h2 className="text-4xl font-semibold tracking-widest text-white">KENT</h2>
-                            <p className="text-3xl font-light text-white">&</p>
-                            <h2 className="text-4xl font-semibold tracking-widest text-white">CJ</h2>
-                            <div className="line w-8 h-[.4px] bg-white"></div>
+                        <div className="banner-title flex flex-col gap-1.5 items-center mb-1">
+                            <h2 className="text-[50px] font-normal tracking-widest leading-12 text-white">ARMAND</h2>
+                            <p className="text-[44px] font-light leading-12 text-white">&</p>
+                            <h2 className="text-[50px] font-normal tracking-widest leading-12 text-white mb-3">EDELYN</h2>
+                            <div className="line w-12 h-[.4px] bg-white"></div>
                         </div>
 
-                        <p className="date text-[10px] tracking-wider text-white">JUNE 20, 2026</p>
+                        <p className="date text-[18px] tracking-wider text-white">JUNE 20, 2026</p>
                     </div>
+                    
+                    <div className="scroll flex flex-col items-center gap-2.5 pb-9">
+                        <h2 className="uppercase text-[14px] tracking-wider text-white">Scroll Down</h2>
+                        <img src="./src/assets/down-chevron.png" className="w-7" alt="Scroll Down" />
+                    </div>
+
                 </div>
             </div>
         </section>
