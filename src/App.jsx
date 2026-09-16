c7ff9ff11e4866cfc7ae952142646730839969df
KenSumang <ksumang77@gmail.com>
2026-09-16 23:47:23 +0800

    Fix minor layout issues on invitation section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,23 @@ import LandingPage from "./pages/LandingPage";
 import InvitationPage from "./pages/MainPage";
 import PageTransition from "./components/PageTransition";
 
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('is-visible');
+      observer.unobserve(entry.target); // Stop watching once visible
+    }
+  });
+}, { threshold: 0.1 });
+
+document.querySelectorAll('.slide-up').forEach(el => {
+  observer.observe(el);
+});
+
+document.querySelectorAll('.slide-up-delay').forEach(el => {
+  observer.observe(el);
+});
+
 function App() {
   return (
     <BrowserRouter>
--- src/components/Invitation.jsx
+++ src/components/Invitation.jsx
@@ -7,12 +7,11 @@ import MapPinIcon from '../assets/map-pin.svg';
 function Invitation() {
     return (
         <section id="invitation" className="invitation w-full h-lvh md:h-full">
-            <div className="container max-w-full h-full px-4 sm:px-6 md:px-10 2xl:px-18 max-w-380">
+            <div className="container max-w-380 mx-auto h-full px-4 sm:px-6 md:px-10 2xl:px-18">
                 <div className="wrapper w-full h-full">
                     <div className="contents w-full h-full grid grid-cols-1 md:my-18 md:grid-cols-5">
                         <div className="description flex flex-col my-auto md:col-span-3 md:h-full md:justify-between">
 
-                            {/* <div className="details-a flex flex-col items-center mx-auto gap-8 mb-10 md:gap-2 md:h-full md:items-start md:mx-0 bg-red-200"> */}
                             <div className="details-a flex flex-col items-center mx-auto gap-8 mb-10 md:h-full md:justify-between md:items-start md:mx-0">
                                 <p className="uppercase text-[14px]">You are invited</p>
 
@@ -28,10 +27,10 @@ function Invitation() {
                             <div className="details-b mx-auto flex flex-wrap sm:flex-nowrap w-full">
                                 <div className="date-time flex mx-auto h-fit justify-between w-full sm:flex-3 md:flex-1">
                                     <div className="date flex-1 flex flex-col gap-3 justify-start items-center lg:justify-center lg:flex-row lg:gap-0 md:justify-between">
-                                        <img src={CalendarIcon} alt="map pin" className="hidden md:block w-5" />
+                                        <img src={CalendarIcon} alt="Calendar" className="hidden md:block w-5" />
                                         <div className="day-and-time md:mr-auto md:ml-[calc(20%-26px)] md:pl-2">
-                                            <p className="uppercase text-[14px] max-w-34 text-center leading-5 h-fit md:hidden">January 15<br/> 2027</p>
-                                            <p className="uppercase hidden text-[14px] max-w-34 text-center leading-5 h-fit md:block lg:text-start">January 15, 2027</p>
+                                            <time dateTime="2027-01-15" className="uppercase text-[14px] max-w-34 text-center leading-5 h-fit md:hidden">January 15<br/> 2027</time>
+                                            <time dateTime="2027-01-15" className="uppercase hidden text-[14px] max-w-34 text-center leading-5 h-fit md:block lg:text-start">January 15, 2027</time>
                                             <p className="uppercase hidden text-[14px] max-w-34 text-center leading-5 h-fit md:block lg:text-start">Saturday</p>
                                         </div>
                                     </div>
@@ -39,7 +38,7 @@ function Invitation() {
                                     <div className="line w-[.5px] h-12 bg-black"></div>
 
                                     <div className="day-time flex-1 flex flex-col gap-3 justify-start items-center lg:justify-center lg:flex-row lg:gap-0">
-                                        <img src={ClockIcon} alt="map pin" className="hidden md:block w-5" /> 
+                                        <img src={ClockIcon} alt="Clock" className="hidden md:block w-5" /> 
                                         <div className="day-and-time md:ml-[calc(20%-26px)] md:pl-2">
                                             <p className="uppercase text-[14px] max-w-34 text-center leading-5 h-fit lg:text-start">4:00 PM</p>
                                             <p className="uppercase text-[14px] max-w-34 text-center leading-5 h-fit md:hidden">Saturday</p>
@@ -50,7 +49,7 @@ function Invitation() {
                                 <div className="line hidden w-[.4px] h-12 bg-black sm:block"></div>
 
                                 <div className="location flex flex-col gap-3 justify-center mt-12 mx-auto sm:mt-0 sm:flex-2 md:flex-1 lg:flex-row lg:gap-0">
-                                    <img src={MapPinIcon} alt="map pin" className="hidden md:block w-6 mx-auto lg:mx-0" />
+                                    <img src={MapPinIcon} alt="Location pin" className="hidden md:block w-6 mx-auto lg:mx-0" />
                                     <p className="uppercase text-center text-[14px] max-w-68 xs:my-auto lg:text-start md:ml-[calc(10%-26px)] md:pl-2">
                                         San Juan Nepomuceno Church San Juan, Batangas
                                     </p>
@@ -60,7 +59,7 @@ function Invitation() {
 
                         <div className="banner hidden md:block bg-red-200 md:col-span-2">
                             <img
-                                className="h-full object-cover"
+                                className="w-full h-full object-cover"
                                 src={InvitationBanner}
                                 alt="" />
                         </div>
