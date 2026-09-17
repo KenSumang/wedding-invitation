e1055c80dea390d24f5b550b38f23c1152b7938c
Mjcapunitan <mjbcapunitan@gmail.com>
2026-09-17 14:40:42 +0800

    updated backup branch

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,9 +1,11 @@
 import Header from '../components/Header';
 import Banner from '../components/Banner';
 import Invitation from '../components/Invitation';
-import CountdownTimer from '../components/CountdownTimer';
+import Rsvp from '../components/Rsvp';
+import Faq from '../components/Faq';
 import { ScheduleOfEvents } from '../components/ScheduleOfEvents';
 import DressCode from '../components/DressCode';
+import Details from '../components/Details';
 
 function MainPage() {
   return (
@@ -11,8 +13,10 @@ function MainPage() {
       <Header />
       <Banner />
       <Invitation />
+      <Details/>
       <DressCode />
-      <Details />
+      <Faq/>
+      <Rsvp/>
       <ScheduleOfEvents />
       {/* <CountdownTimer /> */}
     </>
