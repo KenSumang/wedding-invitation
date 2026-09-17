aeae3335bddf21da9a01a3350abae3d970eb4485
Mjcapunitan <mjbcapunitan@gmail.com>
2026-09-17 13:00:33 +0800

    updated MainPage adding other components

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,9 +1,7 @@
 import Header from '../components/Header';
 import Banner from '../components/Banner';
 import Invitation from '../components/Invitation';
-import Details from '../components/Details';
 import CountdownTimer from '../components/CountdownTimer';
-import { ScheduleOfEvents } from '../components/ScheduleOfEvents';
 import FAQ from '../components/Faq';
 import RSVP from '../components/Rsvp';
 import Timeline from '../components/Timeline';
@@ -14,8 +12,6 @@ function MainPage() {
       <Header />
       <Banner />
       <Invitation />
-      <Details />
-      <ScheduleOfEvents />
       <FAQ/>
       <RSVP/>
       <CountdownTimer />
